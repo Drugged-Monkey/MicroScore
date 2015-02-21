96cec186e08615ad7c1c483c4a276a5f50b030b5
Александр Матюхин <drugged_monkey@outlook.com>
2015-02-21 12:26:27 +0300

    [fix] default tour value for links only with year parameter

--- js/router.js
+++ js/router.js
@@ -10,37 +10,44 @@ define([
         routes: {
             "": "start",
             "!/": "start",
-            '!/year/:id': 'showPage',
+            '!/year/:year': 'showPage',
             '!/year/:year/tour/:tour': 'showPage',
             '*actions': 'defaultAction'
         },
 
-        initialize : function() {
+        initialize: function () {
             self = this;
-            _.bindAll(self, "start", "showPage", "defaultAction")
+            _.bindAll(self,
+                "start",
+                "showPage",
+                "defaultAction")
         },
 
-        start: function() {
-            console.log("app started");
+        start: function () {
+            console.info("[App][Router]:started");
         },
 
         showPage: function (year, tour) {
-            self.model = new PageModel({}, {
-                year: year,
-                tour: _.isUndefined(tour) ? "" : tour 
-            });
-            self.view = new PageView({
-                model: self.model
-            });
+            if (!!!tour) {
+                self.navigate("!/year/" + year + "/tour/" + 1, { trigger: true });
+            } else {
+                self.model = new PageModel({}, {
+                    year: year,
+                    tour: _.isUndefined(tour) ? "" : tour
+                });
+                self.view = new PageView({
+                    model: self.model
+                });
+            }
         },
 
         defaultAction: function (actions) {
-            console.log('No route:', actions);
+            console.error('[App][Router]:There are no corresponing route:', actions);
         }
 
     });
 
-    var initialize = function() {
+    var initialize = function () {
         var router = new AppRouter();
         Backbone.history.start();
         return router;
